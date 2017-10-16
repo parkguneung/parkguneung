813e6337c5e354a1ac210a7b9155ce1ece65578b
parkguneung <rjsdndrnt@naver.com>
2017-10-17 04:31:24 +0900

    맵

---
+++ 프로젝트/맵/배경맵.py
@@ -0,0 +1,4 @@
+from pico2d import *
+open_canvas()
+background=load_image('C:\\github\\프로젝트\\일러스트\\맵일러스트\\밀림(배경맵).png')
+background.draw_now(400,300)
---
+++ 프로젝트/맵/전투씬2(5단계부터).py
@@ -0,0 +1,5 @@
+from pico2d import*
+open_canvas()
+fight1=load_image('C:\\github\\프로젝트\\일러스트\\맵일러스트\\전투씬2 일러스트(5단계부터).png')
+fight1.draw_now(400,300)
+ 
