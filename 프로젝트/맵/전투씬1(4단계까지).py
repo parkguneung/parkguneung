ea5ed87a3e8368c28c30030b014451b26c4e80d0
parkguneung <rjsdndrnt@naver.com>
2017-10-17 16:16:51 +0900

    맵

--- 프로젝트/맵/전투씬1(4단계까지).py
+++ 프로젝트/맵/전투씬1(4단계까지).py
@@ -1,5 +1,5 @@
 from pico2d import*
 open_canvas()
-fight1=load_image('C:\\github\\프로젝트\\일러스트\\맵일러스트\\전투씬1 일러스트(4단계까지).png')
+fight1=load_image('전투씬1 일러스트(4단계까지).png')
 fight1.draw_now(400,300)
  
--- 프로젝트/맵/표지일러스트.py
+++ 프로젝트/맵/표지일러스트.py
@@ -1,5 +1,5 @@
 from pico2d import*
 open_canvas()
-fight1=load_image('C:\\github\\프로젝트\\일러스트\\맵일러스트\\표지일러스트.png')
+fight1=load_image('표지일러스트.png')
 fight1.draw_now(400,300)
  
